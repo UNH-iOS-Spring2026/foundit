efb8e5a9ba21e219ab93db610e600c36bbc52424
Rohan Poudel <nsrapoudel@gmail.com>
2026-03-09 14:20:53 -0500

    Initial Commit

---
+++ foundit/founditApp.swift
@@ -0,0 +1,17 @@
+//
+//  founditApp.swift
+//  foundit
+//
+//  Created by Rohan Poudel on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct founditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
