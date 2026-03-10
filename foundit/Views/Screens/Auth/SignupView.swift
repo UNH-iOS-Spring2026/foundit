5e716d18d73e4331442dc1dc50d7e1851e3b8111
Divya Panthi <panthi.divya03@gmail.com>
2026-03-10 14:42:13 -0400

    create folder structure, add color set and create splash view.

---
+++ foundit/Views/Screens/Auth/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  foundit
+//
+//  Created by Divya Panthi on 10/03/2026.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
