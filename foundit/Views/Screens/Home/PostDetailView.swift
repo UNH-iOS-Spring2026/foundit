139e7bd0a30ba1b2009a5498c2ba52f25a674ff8
Divya Panthi <panthi.divya03@gmail.com>
2026-03-24 12:43:43 -0400

    Create image carousel view to display multiple imaged reported on the post in post detail.

---
+++ foundit/Views/Screens/Home/PostDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailView.swift
+//  foundit
+//
+//  Created by Divya Panthi on 24/03/2026.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDetailView()
+}
