baa185e3f25fa503ebe576363a8f8d8f219ddf56
Divya Panthi <panthi.divya03@gmail.com>
2026-05-09 14:40:05 -0400

    Show default item image in my post item too.

--- foundit/Views/Screens/Profile/MyPostsView.swift
+++ foundit/Views/Screens/Profile/MyPostsView.swift
@@ -394,13 +394,11 @@ private struct MyPostCardView: View {
     }
     
     private var placeholderImage: some View {
-        ZStack {
-            Color(.systemGray5)
-            Image(systemName: "photo")
-                .font(.system(size: 36))
-                .foregroundStyle(Color(.systemGray2))
-        }
-        .frame(width: 110, height: 110)
+        Image("default_item_image")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 110, height: 110)
+            .clipped()
     }
 }
 
